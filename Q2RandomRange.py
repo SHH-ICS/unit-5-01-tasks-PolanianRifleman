449f0d8744aa37f688d5c2951394b75d07f4a6ce
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-04-26 14:31:25 +0000

    code 2

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,24 @@
-# Create a program that accepts 2 numbers from the user. 
-# Your program will output a random number between the range given by the user.
+import random
+
+while True:
+
+    a = input("What is your first value? ")
+
+    try:
+        a = float(a)
+    except ValueError:
+        print("INVALID INPUT. ENTER NUMBER")
+        continue
+
+    b = input("what is your second value? ")
+
+    try:
+        b = float(b)
+    except ValueError:
+        print("INVALID INPUT. ENTER NUMBER")
+        continue
+
+    c = random.randint(a,b)
+
+    print("Your random output is " + str(c))
+    break
