8b2a474eb4055de76d69ce4b5cf3211445b0d3d2
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-04-26 14:50:01 +0000

    meth

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,3 +1,15 @@
 import random
 
-x = 
+while True:
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+
+    z = x + y
+    a = int(input(f"{x} + {y} = "))
+    
+    if a == z:
+        print("Correct!")
+        break
+    else:
+        print("Incorrect")
+        continue
