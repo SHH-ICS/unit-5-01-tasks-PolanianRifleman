3f435ac6111cd1603b78d628563611fa696dd796
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-05-04 10:22:08 -0400

    Update Q4Picture.py

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,25 @@
-# Using the pygame library, draw a simple picture. 
-# It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+mport pygame, sys
+
+pygame.init()
+
+Display = pygame.display.set_mode((500,500))
+
+Clock = pygame.time.Clock()
+FPS = 60
+
+while True:
+    Display.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # Code to render stuff
+    pygame.draw.circle(Display, (255, 0, 0), (250, 250), 50)
+    pygame.draw.circle(Display, (0, 0, 0), (250, 260), 35)
+    pygame.draw.circle(Display, (255, 0, 0), (250, 245), 40)
+    pygame.draw.circle(Display, (0, 0, 0), (230, 245), 15)
+    pygame.draw.circle(Display, (0, 0, 0), (270, 245), 15)
+  
+    pygame.display.flip()
+    Clock.tick(FPS)
